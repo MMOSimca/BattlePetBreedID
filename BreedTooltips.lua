e75a12ef3e7e3789057dde238deffe4fd1cb71e8
Caleb Atherton <MMOSimca@users.noreply.github.com>
2019-10-01 14:04:37 -0400

    Fixed tooltip collision with TradeSkillMaster 4.

--- BreedTooltips.lua
+++ BreedTooltips.lua
@@ -65,21 +65,20 @@ function BPBID_SetBreedTooltip(parent, speciesID, tblBreedID, rareness, tooltipD
     breedtip:SetPoint("TOPRIGHT", parent, "BOTTOMRIGHT", 0, tooltipDistance or 2)
 
     -- Workaround for TradeSkillMaster's tooltip
-    -- Note that setting parent breaks floating tooltip, setting both points breaks borders on TSM tooltip
+    -- Note that setting parent breaks floating tooltip and setting two corner points breaks borders on TSM tooltip
+    -- Setting parent is also required for BattlePetTooltip because TSMExtraTip constantly reanchors itself on its parent
     if (_G.IsAddOnLoaded("TradeSkillMaster")) then
         for i = 1, 10 do
             local t = _G["TSMExtraTip" .. i]
             if t then
-                if (t:GetParent() == BattlePetTooltip) then
+                local _, relativeFrame = t:GetPoint("TOP")
+                if (relativeFrame == BattlePetTooltip) then
                     t:ClearAllPoints()
-                    --t:SetParent(BPBID_BreedTooltip)
-                    t:SetPoint("TOPLEFT", BPBID_BreedTooltip, "BOTTOMLEFT", 0, -1)
-                    --t:SetPoint("TOPRIGHT", BPBID_BreedTooltip, "BOTTOMRIGHT", 0, 1)
+                    t:SetParent(BPBID_BreedTooltip)
+                    t:SetPoint("TOP", BPBID_BreedTooltip, "BOTTOM", 0, -1)
                 elseif (t:GetParent() == FloatingBattlePetTooltip) then
                     t:ClearAllPoints()
-                    --t:SetParent(BPBID_BreedTooltip2)
-                    t:SetPoint("TOPLEFT", BPBID_BreedTooltip2, "BOTTOMLEFT", 0, -1)
-                    --t:SetPoint("TOPRIGHT", BPBID_BreedTooltip2, "BOTTOMRIGHT", 0, 1)
+                    t:SetPoint("TOP", BPBID_BreedTooltip2, "BOTTOM", 0, -1)
                 end
             end
         end
