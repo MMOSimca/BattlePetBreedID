f5a5d8b7677c48199e4d2e30f729aa44712a863f
MMOSimca <MMOSimca@users.noreply.github.com>
2026-02-01 01:56:15 -0500

    Minor styling changes

--- BPBIDAccessOptions.lua
+++ BPBIDAccessOptions.lua
@@ -1,16 +1,16 @@
 -- Get folder path and set addon namespace
 local addonname, internal = ...
-local raw,properName,title,_ = C_AddOns.GetAddOnInfo(addonname)
+local raw, properName, title, _ = C_AddOns.GetAddOnInfo(addonname)
 
 -- Access Style ... pre/post 12.x
-local CatID = GetBPBIDOptionsID()
+local categoryID = GetBPBIDOptionsID()
 
 -- Create slash commands
 SLASH_BATTLEPETBREEDID1 = "/battlepetbreedID"
 SLASH_BATTLEPETBREEDID2 = "/BPBID"
 SLASH_BATTLEPETBREEDID3 = "/breedID"
 SlashCmdList["BATTLEPETBREEDID"] = function(msg)
-    Settings.OpenToCategory(CatID)
+    Settings.OpenToCategory(categoryID)
 end
 
 -- This stuff is only supported in a Retail client
@@ -21,7 +21,7 @@ if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then
 		icon = "Interface/Icons/petjournalportrait.blp",
 		notCheckable = true,
 		func = function(button, menuInputData, menu)
-			Settings.OpenToCategory(CatID)
+			Settings.OpenToCategory(categoryID)
 		end,
 		funcOnEnter = function(button)
 			MenuUtil.ShowTooltip(button, function(tooltip)
